793495d6cc20fb7522b5f43870da7383681e57b6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-23 16:30:27 -0400

    The Centre 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        
         <a href="https://www.facebook.com/bonniedoonsc" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
         <a href="https://www.instagram.com/shopbdc_" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
         <a href="https://www.twitter.com/bonniedoonsc" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
