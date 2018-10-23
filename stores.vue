0436a5018cd5a2b9fac7918a0390165710de687f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-23 18:15:28 -0400

    The Centre 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -41,24 +41,58 @@
 			</div>
 			<div class="row">
 				<div id="store_list_container">
-					<div class="col-xs-6 col-sm-3 col-md-2 cats_row" v-for="store in filteredStores" :data-cat="store.cat_list"  :key="store.id">
-						<div class="store_logo_container" :id="store.initial">
-							<router-link :to="'/stores/'+ store.slug">
-								<img class="store_img" :style="store.initial_img" :src="store.store_front_url_abs"/>
-								<img class="store_hover" :style="store.initial_img" :src="store.hover_img"/>
-								<div class="store_coming_soon" v-if="store.is_coming_soon_store">
-									<div class="new_store">{{$t("stores_page.coming_soon")}}</div>
-								</div>
-								<div class="store_coming_soon" v-if="store.is_new_store">
-									<div class="new_store">{{$t("stores_page.new_store")}}</div>
-								</div>
-								<div class="store_list_promos">
-									<span class="promo_exist" v-if="store.total_published_jobs > 0"><img src="//codecloud.cdn.speedyrails.net/sites/58bdb9106e6f644783090000/image/png/1489000358000/jobs.png" class="" alt=""> {{store.total_published_jobs}}</span>
-									<span class="promo_exist" v-if="store.total_published_promos > 0"><img src="//codecloud.cdn.speedyrails.net/sites/58bdb9106e6f644783090000/image/png/1489000371000/promotions.png" class="" alt=""> {{store.total_published_promos}}</span>
-								</div>
-							</router-link>
-						</div>
-					</div>
+					<!--<div class="col-xs-6 col-sm-3 col-md-2 cats_row" v-for="store in filteredStores" :data-cat="store.cat_list"  :key="store.id">-->
+					<!--	<div class="store_logo_container" :id="store.initial">-->
+					<!--		<router-link :to="'/stores/'+ store.slug">-->
+					<!--			<img class="store_img" :style="store.initial_img" :src="store.store_front_url_abs"/>-->
+					<!--			<img class="store_hover" :style="store.initial_img" :src="store.hover_img"/>-->
+					<!--			<div class="store_coming_soon" v-if="store.is_coming_soon_store">-->
+					<!--				<div class="new_store">{{$t("stores_page.coming_soon")}}</div>-->
+					<!--			</div>-->
+					<!--			<div class="store_coming_soon" v-if="store.is_new_store">-->
+					<!--				<div class="new_store">{{$t("stores_page.new_store")}}</div>-->
+					<!--			</div>-->
+					<!--			<div class="store_list_promos">-->
+					<!--				<span class="promo_exist" v-if="store.total_published_jobs > 0"><img src="//codecloud.cdn.speedyrails.net/sites/58bdb9106e6f644783090000/image/png/1489000358000/jobs.png" class="" alt=""> {{store.total_published_jobs}}</span>-->
+					<!--				<span class="promo_exist" v-if="store.total_published_promos > 0"><img src="//codecloud.cdn.speedyrails.net/sites/58bdb9106e6f644783090000/image/png/1489000371000/promotions.png" class="" alt=""> {{store.total_published_promos}}</span>-->
+					<!--			</div>-->
+					<!--		</router-link>-->
+					<!--	</div>-->
+					<!--</div>-->
+					<div v-masonry transition-duration="0.3s" item-selector=".stores-grid-item" horizontal-order="true">
+                            <transition-group name="custom-classes-transition" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" tag="div">
+                                <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
+                            	    <div class="store_logo_container">
+                            	        <router-link :to="'/stores/'+ store.slug">
+                                			<!--<img class="store_img" :src="store.image_url" alt="">-->
+                                			<div v-if="!store.no_store_logo">
+                                			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png">
+                                			    <img  class="store_img" :src="store.store_front_url_abs" alt="">
+                                			</div>
+                                			
+                                            <div v-else class="no_logo_container">
+                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
+                                                <div class="no_logo_text">
+                                                    <div class="store_text"><h4>{{ store.name }}</h4></div>
+                                                </div>
+                                            </div>
+                                			<div class="store_tag" v-if="store.total_published_promos">
+            									<div class="store_tag_text">Promotion</div>
+            								</div>
+            								<div class="store_tag" v-if="!store.total_published_promos && store.is_coming_soon_store">
+            									<div class="store_tag_text">Coming Soon</div>
+            								</div>
+            								<div class="store_tag" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
+            									<div class="store_tag_text">New Store</div>
+            								</div>
+            								<div class="store_details">
+            								    <div class="store_text"><h4>{{ store.name }}</h4></div>    
+            								</div>
+                                		</router-link>
+                            	    </div>
+                                </div>
+                            </transition-group>
+                        </div>
 				</div>
 			</div>
 		</div>
