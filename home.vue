d82f908286eb5107012e1b0d4214a35b98813af3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-23 15:33:56 -0400

    The Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -127,7 +127,7 @@
                     // return this.$store.state.feature_items;
                     var features = this.$store.state.feature_items;
                     _.forEach(features, function(value, key) {
-                        value.image_url = "https://picsum.photos/200/300?image=98"+key;
+                        value.image_url = "https://picsum.photos/570/570?image=98"+key;
                         
                         if( _.includes([1], key) ) {
                             value.masonry_class = "grid-item--height2";
