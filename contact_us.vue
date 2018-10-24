f004a30b42d6ff1ba8dc2e31f52503578c5fb1cf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-24 15:40:08 -0400

    The Centre 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -89,10 +89,10 @@
                 this.loadData().then(response => {
                     // this.currentPage = response[0].data;
                     if(response && response[0]){
-                        this.main = response[0].data
-                        if(response[0].data && response[0].data.subpages){
-                           this.currentPage = response[0].data.subpages[0];
-                        }
+                        this.currentPage = response[0].data
+                        // if(response[0].data && response[0].data.subpages){
+                        //   this.currentPage = response[0].data.subpages[0];
+                        // }
                     }
                     var temp_repo = this.findRepoByName('Contact Us Banner');
                     if(temp_repo) {
