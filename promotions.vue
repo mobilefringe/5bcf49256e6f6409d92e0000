a0a10bfdc05f024a9d043f836d80d48feed7e8ae
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-24 11:35:39 -0400

    The Centre 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -24,7 +24,6 @@
 							    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h4>
 							    <h4 class="event_store_name caps" v-else>{{promo.store.name_2}}</h4>
 							</router-link>
-							&nbsp;&nbsp;&nbsp;
 							<p class="event_dates">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
 							<p class="event_desc"  v-if="locale=='en-ca'" >{{promo.description_short}}</p>
 							<p class="event_desc" v-else>{{promo.description_short_2}}</p>
