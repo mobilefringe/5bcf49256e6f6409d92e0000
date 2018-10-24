7e2e078dffa240619f5696fde22be4ba6d767c7a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-24 11:50:56 -0400

    The Centre 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="promo_dets_container" v-if="currentPromo">
-		<div class="page_header" v-if="promoBanner" v-bind:style="{ backgroundImage: 'url(' + promoBanner.image_url + ')' }">
+		<div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
 			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
 				<div class="header_content caps">
@@ -76,7 +76,7 @@
                     currentPromo: null,
                     storePromos : null,
                     storeHours : null,
-                    promoBanner : null
+                    pageBanner : null
                 }
             },
             beforeRouteUpdate(to, from, next) {
@@ -91,7 +91,7 @@
                     this.updateCurrentPromo(this.id);
                     var temp_repo = this.findRepoByName('Promos Banner');
                     if(temp_repo) {
-                        this.promoBanner = temp_repo.images[0];
+                        this.pageBanner = temp_repo.images[0];
                     }
                     else {
                         this.pageBanner = {};
