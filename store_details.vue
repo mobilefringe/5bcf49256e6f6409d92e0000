f4e78f773a3d95992b55c0e7c8c5da4b3ba42543
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-26 13:22:56 -0400

    The Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -228,26 +228,32 @@
                     });
                     return store_list;
                 },
-                getPNGurl () {
-                    return "https://www.mallmaverick.com" + this.property.map_url;
+                getSVGMap(){
+                  return "//mallmaverick.com"+this.property.svgmap_url;  
                 },
-                pngMapRef() {
-                    return this.$refs.pngmapref;
-                },
-                floorList() {
+                floorList () {
                     var floor_list = [];
                     
                     var floor_1 = {};
                     floor_1.id = "first-floor";
-                    floor_1.title = "Floor 1";
-                    floor_1.map = this.getPNGurl;
-                    floor_1.z_index = 1;
+                    floor_1.title = "Level One";
+                    floor_1.map = this.getSVGMap;
+                    // floor_1.minimap = this.miniOne;
+                    floor_1.z_index = null;
                     floor_1.show = true;
-                    
                     floor_list.push(floor_1);
+                    
+                    // var floor_2 = {};
+                    // floor_2.id = "second-floor";
+                    // floor_2.title = "Level Two";
+                    // floor_2.map = this.floorTwo;
+                    // // floor_2.minimap = this.miniTwo;
+                    // floor_2.z_index = 2;
+                    // floor_2.show = false;
+                    // floor_list.push(floor_2);
+                    
                     return floor_list;
-                }
-                
+                },
             },
             methods: {
                 loadData: async function() {
