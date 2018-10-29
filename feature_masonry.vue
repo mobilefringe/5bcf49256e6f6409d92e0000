41d4c5186ec6640c2bc61ca92136c6d19b96853b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-29 16:38:50 -0400

    The Centre 1.0  /feature_masonry.vue

--- feature_masonry.vue
+++ feature_masonry.vue
@@ -1,31 +1,31 @@
 <template>
     <div v-masonry transition-duration="0.3s" item-selector=".grid-item" >
-                <div v-masonry-tile class="item feature_item_container" >
-                    <div v-for="feature in feature_items" :class="'grid-item ' + feature.masonry_class ">
-                    	<div  :class="{ 'gallery-image' : feature.no_hover_class}"> 
-                    	<!--class="ih-item circle effect19"-->
-                    		<router-link v-if="feature.url" :to="feature.url">
-                    			<img :src="feature.image_url" alt="name">
-                    			<!--<div class="info">-->
-                    				<div class="figcaption">
-                    					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
-                    					<h3 v-else> {{feature.name_2}} </h3>
-                    				</div>
-                    			<!--</div>-->
-                    		</router-link>
-                    		<div v-else>
-                    			<img :src="feature.image_url" alt="name">
-                    			<!--<div class="info">-->
-                    				<div class="figcaption">
-                    					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
-                    					<h3 v-else> {{feature.name_2}} </h3>
-                    				</div>
-                    			<!--</div>-->
-                    		</div>
-                    	</div>
-                    </div>
-                </div>
+        <div v-masonry-tile class="item feature_item_container" >
+            <div v-for="feature in feature_items" :class="'grid-item ' + feature.masonry_class ">
+            	<div  :class="{ 'gallery-image' : feature.no_hover_class}"> 
+            	<!--class="ih-item circle effect19"-->
+            		<router-link v-if="feature.url" :to="feature.url">
+            			<img :src="feature.image_url" alt="name">
+            			<!--<div class="info">-->
+            				<div class="figcaption">
+            					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
+            					<h3 v-else> {{feature.name_2}} </h3>
+            				</div>
+            			<!--</div>-->
+            		</router-link>
+            		<div v-else>
+            			<img :src="feature.image_url" alt="name">
+            			<!--<div class="info">-->
+            				<div class="figcaption">
+            					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
+            					<h3 v-else> {{feature.name_2}} </h3>
+            				</div>
+            			<!--</div>-->
+            		</div>
+            	</div>
             </div>
+        </div>
+    </div>
     
 </template>
 <style>
