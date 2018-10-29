b4691a72ea6cd1fde4e271030e9d816d739ede73
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-29 16:38:08 -0400

    The Centre 1.0  /feature_masonry.vue

--- feature_masonry.vue
+++ feature_masonry.vue
@@ -13,8 +13,8 @@
     }
 </style>
 <script>
-    define(["Vue", "vuex",  'masonry' , 'vue-masonry-plugin'], function(Vue, Vuex, Lightbox) {
-        Vue.use(Lightbox);
+    define(["Vue", "vuex",  'masonry' , 'vue-masonry-plugin'], function(Vue, Vuex, masonry, VueMasonryPlugin) {
+        Vue.use(VueMasonryPlugin.default);
         return Vue.component("social-links", {
             template: template, // the variable template will be injected,
         });
