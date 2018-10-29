7ff5d4171bd026323c5187c0c8fdd46e1a422ebd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-29 15:51:33 -0400

    The Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -36,7 +36,7 @@
     						<nav id="mobile_nav" v-show="show_mobile_menu">
     							<ul>
     								<div class="mobile_menu_site_logo">
-    									<router-link to="/"><img :src="property_logo" alt="Property Logo"/></router-link>
+    									<router-link to="/"><img :src="inverted_property_logo" alt="Property Logo"/></router-link>
     								</div>
     								<li v-for="(item,key) in menu_items" class="menu_item">
     							        <router-link :to="item.href" v-if="item.sub_menu == undefined">{{$t(item.name)}}</router-link>
