e70d62eee1794f8eaf065e98716c9a0fd4e18416
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-31 14:42:03 -0400

    The Centre 1.0  /utility.js

---
+++ utility.js
@@ -0,0 +1,23 @@
+define([], function () {
+    return {
+        rannumber() {
+            var randnumber = '';
+            for (ranNum = 1; ranNum <= 6; ranNum++) {
+                randnumber += Math.floor(Math.random() * 10).toString();
+            }
+            return randnumber;
+        },
+        serializeObject(obj) {
+            var newObj = [];
+            _.forEach(obj, function(value, key) {
+                var tempVal = {};
+                tempVal.name = key;
+                tempVal.value = value;
+                newObj.push(tempVal);
+            });
+            return newObj;
+        }
+    }
+});
+
+
