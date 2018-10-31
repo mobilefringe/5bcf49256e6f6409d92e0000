474ff91ed784d043b820af96d5e1178d6dd23297
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-31 14:59:13 -0400

    The Centre 1.0  /feature_masonry.vue

--- feature_masonry.vue
+++ feature_masonry.vue
@@ -2,27 +2,24 @@
     <div v-masonry transition-duration="0.3s" item-selector=".grid-item" >
         <div v-masonry-tile class="item feature_item_container" >
             <div v-for="feature in feature_items" :class="'grid-item ' + feature.masonry_class ">
+            <router-link v-if="feature.url" :to="feature.url">
             	<div  :class="{ 'gallery-image' : feature.no_hover_class}"> 
-            	<!--class="ih-item circle effect19"-->
-            		<router-link v-if="feature.url" :to="feature.url">
-            			<img :src="feature.image_url" alt="name">
-            			<!--<div class="info">-->
-            				<div class="figcaption">
-            					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
-            					<h3 v-else> {{feature.name_2}} </h3>
-            				</div>
-            			<!--</div>-->
-            		</router-link>
-            		<div v-else>
-            			<img :src="feature.image_url" alt="name">
-            			<!--<div class="info">-->
-            				<div class="figcaption">
-            					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
-            					<h3 v-else> {{feature.name_2}} </h3>
-            				</div>
-            			<!--</div>-->
-            		</div>
+            	    <img :src="feature.image_url" alt="name">
+    				<div class="figcaption">
+    					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
+    					<h3 v-else> {{feature.name_2}} </h3>
+    				</div>
             	</div>
+        	</router-link>
+            	<div v-else>
+            	    <div  :class="{ 'gallery-image' : feature.no_hover_class}"> 
+            	    <img :src="feature.image_url" alt="name">
+    				<div class="figcaption">
+    					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
+    					<h3 v-else> {{feature.name_2}} </h3>
+    				</div>
+            	</div>
+        		</div>
             </div>
         </div>
     </div>
