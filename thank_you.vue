3955dc4571dda9f6bd48b85922ac81035acd195f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 16:44:09 -0400

    The Centre 1.0  /thank_you.vue

--- thank_you.vue
+++ thank_you.vue
@@ -46,6 +46,17 @@
                     'timezone',
                     'findRepoByName'
                 ])
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([,this.$store.dispatch("getData", "repos")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
             }
         });
     });
