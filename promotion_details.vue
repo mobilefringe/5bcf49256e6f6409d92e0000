06da828aba4d84d8336149c74e5c0144c6923d2e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 17:27:16 -0400

    The Centre 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -66,7 +66,7 @@
 </template>
 
 <script>
-    define(['Vue', 'vuex', 'moment', 'vue-lazy-load', "json!site.json"], function(Vue, Vuex, moment, VueLazyload) {
+    define(['Vue', 'vuex', 'moment', 'vue-lazy-load', "json!site.json"], function(Vue, Vuex, moment, VueLazyload, Site) {
         Vue.use(VueLazyload);
         return Vue.component("promo-details-component", {
             template: template, // the variable template will be injected,
