e6eea66d166b972c5e4b4909d5f88f93e5e2bc96
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 16:09:53 -0400

    The Centre 1.0  /newsletter.vue

---
+++ newsletter.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="page_container" id="contact_us_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div  v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+			<div class="site_container">
+				<div class="header_content">
+					<h1>{{$t("newsletter_page.newsletter")}}</h1>
+				</div>
+			</div>
+		</div>  
+        <div class="site_container">
+            <div class="row"> 
+                <div class="col-md-12 contact_contents">
+                    <form class="form-horizontal" action="https://mobilefringe.createsend.com/t/d/s/silhht/" method="post" @submit.prevent="validateBeforeSubmit">
+                        <div class="form-group ">
+                            <div class="col-sm-6 col-xs-12" >
+                                <label class="label" for="cm-name">{{$t("newsletter_page.name")}}</label>
+                                <input v-model="form_data.name" required class="form-control" name="cm-name" type="text" placeholder="Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-6 col-xs-12">
+                                <label class="label" for="cm-irudui-irudui">{{$t("newsletter_page.email")}}</label>
+                                <input v-model="form_data.email" required class="form-control" name="cm-silhht-silhht" type="email" placeholder="Email" id="newsletter_email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-xs-12">
+    					        <label class="checkbox">
+                                    <input name="agree_newsletter" required  type="checkbox">
+                                        {{$t("newsletter_page.agree")}} {{property.name}}. 
+                                </label>
+    					    </div>
+    					</div>
+    					<div class="form-group">
+                            <div class="col-xs-12">
+                                <button class="contest_btn" type="submit" :disabled="formSuccess">{{$t("newsletter_page.subscribe")}}</button>
+                            </div>
+                        </div>
+                    </form>
+                    
+                    <div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">
+                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                        <span class="sr-only">{{$t("newsletter_page.success")}} : </span>
+                        {{$t("newsletter_page.thank_you_message")}}
+                    </div>
+                    <div id="send_contact_error" class="alert alert-danger" role="alert" v-show="formError">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                        <span class="sr-only">{{$t("newsletter_page.error")}} : </span>
+                        {{$t("newsletter_page.error_message")}}
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="padding_top_40"></div>    
+        </div>
+    </div>
+</template>
+<style>
+    .form-group label {
+        display: inline-block;
+    }
+    .checkbox {
+        font-weight: normal;
+            margin-left: 20px;
+    }
+</style>
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate', 'jquery', 'utility'], function(Vue, Vuex, moment, tz, VueMoment, Meta, VeeValidate, $, Utility) {
+        Vue.use(Meta);
+        Vue.use(VeeValidate);
+        return Vue.component("newsletter-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    currentPage: null,
+                    form_data : {},
+                    formSuccess : false,
+                    formError: false,
+                    pageBanner: null
+                }
+            },
+            created () {
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Newsletter Banner');
+                    if(temp_repo) {
+                        this.pageBanner = temp_repo.images[0];
+                    }
+                    // this.pageBanner = this.findRepoByName('Contact Us Banner').images[0];
+                   console.log(this.pageBanner); 
+                });    
+            },
+            mounted () {
+                this.form_data.email = this.$route.query.email;
+                $("#newsletter_email").val(this.form_data.email);
+            },
+            watch : {
+                $route () {
+                    this.form_data.email = this.$route.query.email;
+                    $("#newsletter_email").val(this.form_data.email);
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName'
+                ])
+            },
+            methods: {
+                validateBeforeSubmit(form) {
+                    this.$validator.validateAll().then((result) => {
+                        if (result) {
+                            let errors = this.errors;
+                            
+                            if(errors.length > 0) {
+                                console.log("Error");
+                                this.formError = true;
+                            }
+                            else {
+                                form.preventDefault();
+                                console.log("No Error", form);
+                                var vm = this;
+                                $.getJSON(
+                                form.target.action + "?callback=?",
+                                $(form.target).serialize(),
+                                function (data) {
+                                    if (data.Status === 400) {
+                                       vm.formError = true;
+                                    } else { // 200
+                                        vm.formSuccess = true;
+                                    }
+                                });
+                                
+                            }
+                        }
+                    })
+                },
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([,this.$store.dispatch("getData", "repos")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+            }
+        });
+    });
+</script>
