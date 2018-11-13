1f53f5c732c748181499936e6f65c22240aaf667
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-11-13 15:13:32 -0500

    The Centre 1.0  /feature_masonry.vue

--- feature_masonry.vue
+++ feature_masonry.vue
@@ -12,8 +12,8 @@
                 	</div>
             	</router-link>
             	<div v-else>
-            	    <div  :class="{ 'gallery-image' : feature.no_hover_class}"> 
-                	    <img :src="feature.image_url" alt="name">
+            	    <div :class="{ 'gallery-image' : feature.no_hover_class}"> 
+                	    <img :src="feature.image_url" :alt="feature.name">
         				<div class="figcaption">
         					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
         					<h3 v-else> {{feature.name_2}} </h3>
