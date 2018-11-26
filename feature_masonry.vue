74ac8e15685ee8c54412fcabd71f10a7c0cf4e63
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-26 11:23:08 -0500

    The Centre 1.0  /feature_masonry.vue

--- feature_masonry.vue
+++ feature_masonry.vue
@@ -6,7 +6,7 @@
                 	<div :class="{ 'gallery-image' : feature.no_hover_class}"> 
                 	    <img :src="feature.image_url" :alt="feature.name">
         				<div class="figcaption">
-        					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
+        					<h3 v-if="locale=='en-ca'" class="hvr-underline-from-center"> {{feature.name}} </h3>
         					<h3 v-else> {{feature.name_2}} </h3>
         					<!--<div class=""></div>-->
         				</div>
