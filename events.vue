77adc77d8dc864c51dd7fbe73c59245dfa1c01c3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-26 15:29:08 -0500

    The Centre 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -190,7 +190,7 @@
                             temp_promo.push(value);
                         }
                     });
-                    temp_promo = _.sortBy(temp_promo, [function(o) { return o.start_date; }]);
+                    temp_promo = _.sortBy(temp_promo, ['start_date', 'end_date']);//_.sortBy(temp_promo, [function(o) { return o.start_date; }]);
                     return temp_promo;
                 },
             },
