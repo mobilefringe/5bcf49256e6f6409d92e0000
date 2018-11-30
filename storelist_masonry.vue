4d2021c93eac69cba38f5fdc80c8f827b5ae4c54
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-30 14:21:49 -0500

    The Centre 1.0  /storelist_masonry.vue

--- storelist_masonry.vue
+++ storelist_masonry.vue
@@ -12,7 +12,7 @@
                         <div v-else class="no_logo_container">
                             <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                             <div class="no_logo_text">
-                                <div class="store_text"><h4>{{ store.name }}</h4></div>
+                                <div class="store_text"><h3>{{ store.name }}</h3></div>
                             </div>
                         </div>
             			<div class="store_tag" v-if="store.total_published_promos">
@@ -25,7 +25,7 @@
 							<div class="store_tag_text">New Store</div>
 						</div>
 						<div class="store_details">
-						    <div class="store_text"><h4>{{ store.name }}</h4></div>    
+						    <div class="store_text"><h3>{{ store.name }}</h3></div>    
 						</div>
             		</router-link>
         	    </div>
