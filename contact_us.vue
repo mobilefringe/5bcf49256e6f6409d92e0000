4ffbfacef9c830a0c2276755415590bfc4d58fd4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-30 17:02:45 -0500

    The Centre 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -25,7 +25,7 @@
                             </div>
                             <div class="col-sm-12" :class="{'has-error': errors.has('email')}">
                                 <label class="label" for="email" style="display:none;">Email</label>
-                                <input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="email@email.com" data-vv-delay="500">
+                                <input id="email" v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="email@email.com" data-vv-delay="500">
                                 <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
                             </div>
                             <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
